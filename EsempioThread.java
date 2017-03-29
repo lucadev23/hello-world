36d5dc97940fac5a701631df654cf38f910c6dd3
lucadev23 <luca.mer13@gmail.com>
2017-03-29 23:00:06 +0200

    Update EsempioThread.java

--- EsempioThread.java
+++ EsempioThread.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package esempiothread;
 
 /**
@@ -11,11 +6,8 @@ package esempiothread;
  */
 public class EsempioThread {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         MainForm mf=new MainForm();
         mf.setVisible(true);
         
