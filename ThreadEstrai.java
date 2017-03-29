6bc5ed5ebca1abfa577367f2418e3af536d03a3b
lucadev23 <luca.mer13@gmail.com>
2017-03-29 23:02:20 +0200

    Add files via upload
    An example of thread

---
+++ ThreadEstrai.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esempiothread;
+
+/**
+ *
+ * @author alegu
+ */
+public class ThreadEstrai extends Thread{
+    private MainForm mf;
+    private int millSec;
+    
+    public ThreadEstrai(MainForm mf,int t)
+    {
+        this.mf=mf;
+        millSec=t;
+    }
+    
+    
+    public void run()
+    {
+      try
+      {  aggiorna();  new IllegalArgumentException("timeout value is negative");}
+      catch (Exception e)
+      {  
+          e.printStackTrace();
+          System.out.println("Errore nel Thread mio");
+      }
+    }
+    
+    public void aggiorna ()
+    {
+        
+        while(true)
+        {
+              
+                try
+                {
+                Thread.sleep(millSec);//1 minuto=60000
+                mf.generaNumero();
+                }catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+        }
+   }
+    
+}
