b4228d2411de338eec566216346ed35bf5f88ce6
lucadev23 <luca.mer13@gmail.com>
2017-03-29 23:09:03 +0200

    Update ThreadEstrai.java

--- ThreadEstrai.java
+++ ThreadEstrai.java
@@ -1,50 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package esempiothread;
-
 /**
  *
  * @author lucadev23
  */
-public class ThreadEstrai extends Thread{
-    private MainForm mf;
+public class ThreadExample extends Thread{
     private int millSec;
     
-    public ThreadEstrai(MainForm mf,int t)
-    {
-        this.mf=mf;
-        millSec=t;
+    public ThreadExample(int time) { //time in milliseconds
+        millSec=time;
     }
     
-    
-    public void run()
-    {
-      try
-      {  aggiorna();  new IllegalArgumentException("timeout value is negative");}
-      catch (Exception e)
+    public void run() {
+      try {  
+          update();
+          new IllegalArgumentException("timeout value is negative");
+      } catch (Exception e)
       {  
           e.printStackTrace();
-          System.out.println("Errore nel Thread mio");
+          System.out.println("Error Thread");
       }
     }
     
-    public void aggiorna ()
-    {
-        
-        while(true)
-        {
-              
-                try
-                {
-                Thread.sleep(millSec);//1 minuto=60000
-                mf.generaNumero();
-                }catch (Exception e)
-                {
-                    e.printStackTrace();
-                }
+    public void update () {
+        while(true) {
+            try {
+                Thread.sleep(millSec);//1 minute=60000
+                //Anything you want to do...
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
 
         }
    }
